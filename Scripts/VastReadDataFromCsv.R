4954c01370128d053743cdf89bf8b796bac8da20
Ravi <ravi419@github.com>
2020-05-16 16:07:45 +0200

    added comments to the files

--- Scripts/VastReadDataFromCsv.R
+++ Scripts/VastReadDataFromCsv.R
@@ -1,33 +1,16 @@
 ## Read all csv files and write them into 1 single file
 library(dplyr)
 library(tidyverse)
-# processFile <- function(f) {
-#   df <- read.csv(f)
-#   # ...and do stuff...
-#   combined_df <- rbind(combined_df,df)
-#   file.info(f)$size # dummy result
-# }
-# processFile <- function(f) {
-#   df <- read.csv(f)
-#   # ...and do stuff...
-#   b <- tools::file_path_sans_ext(basename(f)
-#   df <- df %>% add_column(filenames = b , .before = 1)
-#   file.info(f)$size # dummy result
-#   DF <- rbind(DF,df)
-# }
-
-
-
 
 # Find all .csv files
 files <- dir("~/VAST_Challenge/MC2-Image-Data", recursive=TRUE, full.names=TRUE, pattern="\\.csv$")
 DF <-  read.csv(files[1])
+# Get the filename 
 a <- tools::file_path_sans_ext(basename(files[1]))
+# Append the filename as the first column of the DF
 DF <- DF %>% add_column(filenames = a , .before = 1)
-#result <- sapply(files[-1], processFile)
-#files
 
-
+# For all he files repeat the same as above and append everything to the DF 
 for (i in files[-1]){
 
   df <- read.csv(i)      # read the file
@@ -35,10 +18,6 @@ for (i in files[-1]){
   df <- df %>% add_column(filenames = a , .before = 1)
   DF <- rbind(DF, df)    # append the current file
 }
+
+# Writ it to the csv file
 write.csv(DF, "~/VAST_Challenge/VAST-Mini-Challenge2/Data/Yolo_persons_appended.csv", row.names=FALSE, quote=FALSE)
-# f <- data.frame(f)
-# #writing the appended file  
-
-# # Apply the function to all files.
-
-# write.csv(result, "Models_appended.csv", row.names=FALSE, quote=FALSE)
